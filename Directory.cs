1c04ee2b0a56ea708d02f1a03846c58b8279775c
Frederik Gelder <frederik.gelder@freenet.de>
2018-05-27 23:12:11 +0200

    refactoring variable names

--- Directory.cs
+++ Directory.cs
@@ -6,23 +6,23 @@ namespace Thorium.IO
 {
     public static class Directory
     {
-        public static void CopyDirectory(string source, string target)
+        public static void CopyDirectory(string source, string destination)
         {
             var stack = new Stack<Tuple<string, string>>();
-            stack.Push(new Tuple<string, string>(source, target));
+            stack.Push(new Tuple<string, string>(source, destination));
 
             while(stack.Count > 0)
             {
-                var folders = stack.Pop();
-                System.IO.Directory.CreateDirectory(folders.Item2);
-                foreach(var file in System.IO.Directory.GetFiles(folders.Item1, "*.*", SearchOption.TopDirectoryOnly))
+                var sourceAndDestination = stack.Pop();
+                System.IO.Directory.CreateDirectory(sourceAndDestination.Item2);
+                foreach(var file in System.IO.Directory.GetFiles(sourceAndDestination.Item1, "*.*", SearchOption.TopDirectoryOnly))
                 {
-                    File.Copy(file, Path.Combine(folders.Item2, Path.GetFileName(file)));
+                    File.Copy(file, Path.Combine(sourceAndDestination.Item2, Path.GetFileName(file)));
                 }
 
-                foreach(var folder in System.IO.Directory.GetDirectories(folders.Item1))
+                foreach(var folder in System.IO.Directory.GetDirectories(sourceAndDestination.Item1))
                 {
-                    stack.Push(new Tuple<string, string>(folder, Path.Combine(folders.Item2, Path.GetFileName(folder))));
+                    stack.Push(new Tuple<string, string>(folder, Path.Combine(sourceAndDestination.Item2, Path.GetFileName(folder))));
                 }
             }
         }
